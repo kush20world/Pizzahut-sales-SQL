a2d4ec56ee9d966ff358f8b724d4a07e0822cca7
kush20world <142404468+kush20world@users.noreply.github.com>
2024-09-26 16:23:44 +0530

    Add files via upload

---
+++ SQL pizza sales/pizza_sales/pizza_scr_1.sql
@@ -0,0 +1,3 @@
+-- retrive the total numbers of order placed.
+
+select count(order_id) as  total_order from orders;
---
+++ SQL pizza sales/pizza_sales/pizza_scr_8.sql
@@ -0,0 +1,8 @@
+-- join the relevant table to find the category wise distribution of pizzas.
+
+SELECT 
+    category, COUNT(name)
+FROM
+    pizza_types
+GROUP BY category;
+
